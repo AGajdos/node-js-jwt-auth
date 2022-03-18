de032c316cbc4d5a0f5480791da24ce51ba10912
tanulo <tanulo@barossgabor.local>
2022-03-18 13:56:18 +0100

    08.18

--- app/routes/sajatbackend.js
+++ app/routes/sajatbackend.js
@@ -1,5 +1,6 @@
 const { authJwt } = require("../middleware");
 const controller = require("../controllers/user.controller");
+const fileupload = require("express-fileupload");
 
 module.exports = function(app) {
   app.use(function(req, res, next) {
@@ -22,6 +23,29 @@ app.get('/gyakorlatok', (req, res) => {
   connection.connect()
   
   connection.query('SELECT * from gyakorlatok ', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows)
+
+    res.send(rows)
+  })
+  
+  
+  connection.end()    
+
+})
+app.get('/video', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT * from video ', function (err, rows, fields) {
     if (err) throw err
   
     console.log(rows)
@@ -56,7 +80,7 @@ app.get('/gyakorlatok_mell', (req, res) => {
   connection.end()    
 
 })
-app.get('/izomcsoport', (req, res) => {
+app.get('/izom', (req, res) => {
   var mysql = require('mysql')
   var connection = mysql.createConnection({
     host: 'localhost',
@@ -126,6 +150,30 @@ app.post('/torles2', (req, res) => {
   connection.end()    
 
 })
+app.post('/torles3', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+  
+  connection.query('Delete from forum where k_id= '+req.body.bevitel1, function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows)
+
+    res.send("Sikeres törlés")
+  })
+  
+  
+  connection.end()    
+
+})
+
 app.post('/kommentfelvitel', (req, res) => {
   var mysql = require('mysql')
   var connection = mysql.createConnection({
@@ -173,5 +221,259 @@ app.get('/tema', (req, res) => {
 
 })
 
+
+app.post('/adatfelvitel', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+  
+  
+  connection.query("INSERT INTO gyakorlatok VALUES (NULL,'"+req.body.bevitel1+"', '"+req.body.bevitel2+"','"+req.body.bevitel3+"','"+req.body.bevitel4+"' ) ", function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log("Sikeres felvitel!")
+
+    res.send("Sikeres felvitel!")
+  })
+  
+  connection.end()    
+
+})  
+
+
+app.post('/adatfelvitel2', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+  
+  
+  connection.query("INSERT INTO gyakorlatok VALUES (NULL,'"+req.body.bevitel1+"', '"+req.body.bevitel2+"','"+req.body.bevitel3+"','"+req.body.bevitel4+"' ) ", function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log("Sikeres felvitel!")
+
+    res.send("Sikeres felvitel!")
+  })
+  
+  connection.end()    
+
+})  
+
+
+app.get('/izomcsoport', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT * from izomcsoport', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows)
+
+    res.send(rows)
+  })
+  
+  
+  connection.end()    
+
+})
+
+app.get('/gyakorlatok_hat', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT * from gyakorlatok WHERE izom_gyakorlat_id=2', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows)
+
+    res.send(rows)
+  })
+  
+  
+  connection.end()    
+
+})
+app.get('/gyakorlatok_vall', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT * from gyakorlatok WHERE izom_gyakorlat_id=3', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows)
+
+    res.send(rows)
+  })
+  
+  
+  connection.end()    
+
+})
+
+app.get('/gyakorlatok_tricepsz', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT * from gyakorlatok WHERE izom_gyakorlat_id=5', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows)
+
+    res.send(rows)
+  })
+  
+  
+  connection.end()    
+
+})
+
+app.get('/gyakorlatok_bicepsz', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT * from gyakorlatok WHERE izom_gyakorlat_id=4', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows)
+
+    res.send(rows)
+  })
+  
+  
+  connection.end()    
+
+})
+
+app.get('/gyakorlatok_comb', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT * from gyakorlatok WHERE izom_gyakorlat_id=6', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows)
+
+    res.send(rows)
+  })
+  
+  
+  connection.end()    
+
+})
+app.get('/gyakorlatok_vadli', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT * from gyakorlatok WHERE izom_gyakorlat_id=7', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows)
+
+    res.send(rows)
+  })
+  
+  
+  connection.end()    
+
+})
+app.post('/kereses', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+  var feltetel2='gyakorlat_nev LIKE "%'+req.body.bevitel1+'%"';
+  
+  connection.query('SELECT gyakorlat_nev,gyakorlat_leiras,gyakorlat_kep FROM gyakorlatok WHERE '+feltetel2, function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows)
+
+    res.send(rows)
+  })
+  
+  
+  connection.end()    
+
+})
+
+app.use(fileupload());
+  app.post("/upload", (req, res) => {
+    const newpath = "./kepek/";
+    const file = req.files.file;
+    const filename = file.name;
+  
+    file.mv(`${newpath}${filename}`, (err) => {
+      if (err) {
+        return res.status(500).send({ message: "File upload failed", code: 200 });
+      }
+        return res.status(200).send({ message: "File Uploaded", code: 200 });
+    });
+  });
 
 };
